9938abcef0c685413c9df8f711086a7d7bf9e386
yehonatan20 <yehonatan20@gmail.com>
2025-02-24 10:34:31 +0200

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Helmet } from 'react-helmet-async'
+import Title from './components/shared/Title'
 
 function App() {
 
   return (
     <>
-      
+      <Title title="Home page"/>
     </>
   )
 }
---
+++ frontend/src/components/shared/Title.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Helmet } from 'react-helmet-async'
+import PropTypes from 'prop-types'
+
+const Title = ({title}) => {
+  return (
+    <Helmet>
+        <title>{title}</title>
+    </Helmet>
+  )
+}
+
+Title.propTypes = {
+    title: PropTypes.string
+}
+
+export default Title
